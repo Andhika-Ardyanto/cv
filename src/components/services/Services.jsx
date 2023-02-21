3da03e71899c0f47c78064120adf866f9832c9be
Andhika-Ardyanto <andhikaardyanto114@gmail.com>
2023-02-21 20:08:48 +0700

    first commit

---
+++ src/components/services/Services.jsx
@@ -0,0 +1,90 @@
+import React from 'react'
+import './services.css'
+import {BiCheck} from 'react-icons/bi'
+
+const Services = () => {
+  return (
+    <section id='services'>
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services__container">
+        <article className='service'>
+          <div className="service__head">
+            <h3>Mobile Application Android & Ios</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Online Store</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>attendance</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Movie Apps</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>daily expenses</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Quizz</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Mini Games</p>
+            </li>
+          </ul>
+        </article>
+        {/* End Mobile */}
+        <article className='service'>
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Online Store</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>attendance</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Movie Apps</p>
+            </li>
+          </ul>
+        </article>
+        {/* End WEB */}
+        <article className='service'>
+          <div className="service__head">
+            <h3>UI/UX Design</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Online Store</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Movie Apps</p>
+            </li>
+            
+          </ul>
+        </article>
+      </div>
+      
+    
+    </section>
+  )
+}
+
+export default Services
